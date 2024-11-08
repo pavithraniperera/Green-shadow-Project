741e6349787ceccdaf8e6280b03cb088640f88bf
pavithrani <pererapavithrani02@gmail.com>
2024-11-08 07:13:04 +0530

    create spring boot project for backend

---
+++ BackEnd/GreenShadow-Bacend/src/main/java/lk/ijse/greenshadowbacend/GreenShadowBacendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.greenshadowbacend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenShadowBacendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenShadowBacendApplication.class, args);
+    }
+
+}
---
+++ BackEnd/GreenShadow-Bacend/src/test/java/lk/ijse/greenshadowbacend/GreenShadowBacendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.greenshadowbacend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenShadowBacendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
