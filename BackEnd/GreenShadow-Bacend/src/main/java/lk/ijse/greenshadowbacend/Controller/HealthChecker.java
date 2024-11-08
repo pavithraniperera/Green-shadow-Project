3389bfd29369893939ef5ccc8c405a1e2e54c3bc
pavithrani <pererapavithrani02@gmail.com>
2024-11-08 07:37:28 +0530

    create Health checker for testing

---
+++ BackEnd/GreenShadow-Bacend/src/main/java/lk/ijse/greenshadowbacend/Controller/HealthChecker.java
@@ -0,0 +1,15 @@
+package lk.ijse.greenshadowbacend.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("api/v1/health")
+public class HealthChecker {
+
+        @GetMapping
+        public String healthTest(){
+            return "Green shadow controller working";
+        }
+
+}
