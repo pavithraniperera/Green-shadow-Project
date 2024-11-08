4904cc8b2db2df669c5d24eec9df45a92a89d548
pavithrani <pererapavithrani02@gmail.com>
2024-11-08 19:52:14 +0530

    implement staff save and getbyEmail method

--- BackEnd/GreenShadow-Bacend/src/main/java/lk/ijse/greenshadowbacend/Service/impl/StaffServiceImpl.java
+++ BackEnd/GreenShadow-Bacend/src/main/java/lk/ijse/greenshadowbacend/Service/impl/StaffServiceImpl.java
@@ -1,17 +1,36 @@
 package lk.ijse.greenshadowbacend.Service.impl;
 
+import lk.ijse.greenshadowbacend.Dao.StaffDao;
 import lk.ijse.greenshadowbacend.Dto.impl.StaffDto;
+import lk.ijse.greenshadowbacend.Entity.StaffEntity;
 import lk.ijse.greenshadowbacend.Service.StaffService;
+import lk.ijse.greenshadowbacend.Util.AppUtil;
+import lk.ijse.greenshadowbacend.Util.Mapping;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 @Transactional
 public class StaffServiceImpl implements StaffService {
+    @Autowired
+    private StaffDao staffDao;
+
+    @Autowired
+    private Mapping staffMapper;
     @Override
     public StaffDto save(StaffDto dto) {
-        return null;
+       dto.setStaffId(AppUtil.generateStaffId());
+        StaffEntity save = staffDao.save(staffMapper.toStaffEntity(dto));
+        if(save==null){
+            System.out.println("not saved staff data");
+            //throw new DataPersistException(" Staff not saved");
+        }
+        return staffMapper.toStaffDto(save);
     }
 
     @Override
@@ -32,5 +51,13 @@ public class StaffServiceImpl implements StaffService {
     @Override
     public List<StaffDto> findAll() {
         return null;
+    }
+
+    @Override
+    public Optional<StaffDto> findByEmail(String email) {
+        Optional<StaffEntity> byEmail = staffDao.findByEmail(email);
+
+        return byEmail.map(staffMapper::toStaffDto);
+
     }
 }
