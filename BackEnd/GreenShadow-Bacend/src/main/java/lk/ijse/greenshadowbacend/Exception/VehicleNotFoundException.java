fde0d34ac504ae63ff6d25ee47759329e57f0af0
pavithrani <pererapavithrani02@gmail.com>
2024-11-11 07:26:40 +0530

    implement vehicle custom exception

---
+++ BackEnd/GreenShadow-Bacend/src/main/java/lk/ijse/greenshadowbacend/Exception/VehicleNotFoundException.java
@@ -0,0 +1,15 @@
+package lk.ijse.greenshadowbacend.Exception;
+
+public class VehicleNotFoundException extends RuntimeException{
+    public VehicleNotFoundException() {
+        super();
+    }
+
+    public VehicleNotFoundException(String message) {
+        super(message);
+    }
+
+    public VehicleNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
