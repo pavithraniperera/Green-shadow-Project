ba2ea816bf8e2ca013b0e3f39c9ed3db76e42dde
pavithrani <pererapavithrani02@gmail.com>
2024-11-13 12:42:30 +0530

    remove save user method

--- BackEnd/GreenShadow-Bacend/src/main/java/lk/ijse/greenshadowbacend/Controller/UserController.java
+++ BackEnd/GreenShadow-Bacend/src/main/java/lk/ijse/greenshadowbacend/Controller/UserController.java
@@ -21,39 +21,6 @@ import java.util.regex.Pattern;
 public class UserController {
     @Autowired
     private UserService userService;
-    @Autowired
-    private StaffService staffService;
-
-    @PostMapping( consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> createUser(@RequestBody UserDto userDto) {
-        try {
-            // Check if a staff member exists with the given email
-            Optional<StaffDto> existingStaff = staffService.findByEmail(userDto.getEmail());
-
-            if (!existingStaff.isPresent()) {
-                // Save new staff member if none exists
-                StaffDto newStaff = new StaffDto();
-                newStaff.setEmail(userDto.getEmail());
-                newStaff.setRole(userDto.getRole());
-
-
-                newStaff = staffService.save(newStaff);
-
-                // Set the saved staff ID to the user DTO
-                userDto.setStaffId(newStaff.getStaffId());
-            } else {
-                // Link to the existing staff member
-                userDto.setStaffId(existingStaff.get().getStaffId());
-            }
-
-            // Save the user
-            userService.save(userDto);
-            return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully.");
-
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating user: " + e.getMessage());
-        }
-    }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<UserDto> getAllUsers(){
