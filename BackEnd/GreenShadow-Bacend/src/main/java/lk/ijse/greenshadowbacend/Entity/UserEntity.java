a9e1933859d0b9cf24de5b39965979195d678c9e
pavithrani <pererapavithrani02@gmail.com>
2024-11-13 07:33:04 +0530

    implement userDetails to user entity

--- BackEnd/GreenShadow-Bacend/src/main/java/lk/ijse/greenshadowbacend/Entity/UserEntity.java
+++ BackEnd/GreenShadow-Bacend/src/main/java/lk/ijse/greenshadowbacend/Entity/UserEntity.java
@@ -4,13 +4,20 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "user")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id
     private String userId;
     @Column(unique = true)
@@ -23,4 +30,35 @@ public class UserEntity {
     @JoinColumn(name = "staff_id")
     private StaffEntity staff;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_"+role.name()));
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
