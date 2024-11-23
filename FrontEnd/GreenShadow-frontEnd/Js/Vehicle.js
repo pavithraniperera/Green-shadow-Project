522368706f0ce7dc4e2d6ca9e52af70927b745c3
pavithrani <pererapavithrani02@gmail.com>
2024-11-23 10:53:18 +0530

    fetch staff ids for vehicles

--- FrontEnd/GreenShadow-frontEnd/Js/Vehicle.js
+++ FrontEnd/GreenShadow-frontEnd/Js/Vehicle.js
@@ -54,21 +54,20 @@ function toggleVehicleEditMode() {
 
 }
 function addStaff() {
+    const existingOptions = $('.staffForVehicle:first').html();
     const container = document.getElementById("assignedStaffContainer");
     const staffDiv = document.createElement("div");
     staffDiv.className = "d-flex align-items-center mb-2";
     staffDiv.innerHTML = `
-        <select class="form-control glass-input mr-2" name="assignedStaff[]" required>
-            <option value="">Select Staff</option>
-            <option value="Staff Member 1">Staff Member 1</option>
-            <option value="Staff Member 2">Staff Member 2</option>
-            <!-- Add more options here if needed -->
+        <select class="form-control glass-input mr-2 staffForVehicle" name="assignedStaff[]" required>
+            ${existingOptions || '<option value="">Select Staff Member</option>'}
         </select>
         <button type="button" class="btn btn-sm custom-btn" onclick="removeStaff(this)">
             <i class="fa-regular fa-trash-can" style="color: green"></i>
         </button>
     `;
     container.appendChild(staffDiv);
+
 }
 
 function removeStaff(button) {
@@ -78,3 +77,36 @@ function removeStaff(button) {
 function clearVehicleForm() {
     document.getElementById("addVehicleForm").reset();
 }
+
+
+function fetchStaffForVehicle(){
+    $.ajax({
+        url: "http://localhost:8080/greenShadow/api/v1/staffs", // Update with your actual endpoint
+        type: "GET",
+        headers: {
+            "Authorization": "Bearer " + localStorage.getItem("token")
+        },
+        success: function (staffList) {
+            // Assuming response is an array of FieldDto objects
+            const staffSelect = $("#assignedStaff");
+            staffSelect.empty();
+            staffSelect.append('<option value="">Select Staff</option>'); // Default option
+
+            // Populate the select element with fetched staff and pre-select assigned ones
+            staffList.forEach(staff => {
+
+                const option = `<option value="${staff.staffId}">${staff.firstName}</option>`;
+                staffSelect.append(option);
+            });
+        },
+        error: function (error) {
+            console.error("Error fetching Staff:", error);
+            showAlert("Failed to load staff. Please try again later.",'error');
+        }
+    });
+}
+$("#addVehicle").click(function () {
+    fetchStaffForVehicle()
+})
+
+
