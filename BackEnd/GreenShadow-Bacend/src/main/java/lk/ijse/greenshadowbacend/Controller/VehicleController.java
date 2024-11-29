133be44cd1390f48e214e0ef407f0ebf59716837
pavithrani <pererapavithrani02@gmail.com>
2024-11-29 12:00:13 +0530

    update vehicle controllers with adding loggers

--- BackEnd/GreenShadow-Bacend/src/main/java/lk/ijse/greenshadowbacend/Controller/VehicleController.java
+++ BackEnd/GreenShadow-Bacend/src/main/java/lk/ijse/greenshadowbacend/Controller/VehicleController.java
@@ -1,11 +1,11 @@
 package lk.ijse.greenshadowbacend.Controller;
 
-import lk.ijse.greenshadowbacend.Dto.impl.StaffDto;
 import lk.ijse.greenshadowbacend.Dto.impl.VehicleDto;
-import lk.ijse.greenshadowbacend.Exception.StaffNotFoundException;
 import lk.ijse.greenshadowbacend.Exception.VehicleNotFoundException;
 import lk.ijse.greenshadowbacend.Service.VehicleService;
 import lk.ijse.greenshadowbacend.Util.RegexUtilForId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,23 +20,26 @@ import java.util.List;
 public class VehicleController {
     @Autowired
     private VehicleService vehicleService;
+    private static final Logger log = LoggerFactory.getLogger(VehicleController.class);
 
     // Save Vehicle
     @PostMapping
     public ResponseEntity<VehicleDto> saveVehicle(@RequestBody VehicleDto vehicleDto) {
-        System.out.println(vehicleDto);
+        log.info("Request received to save vehicle: {}", vehicleDto);
         VehicleDto savedVehicle = vehicleService.save(vehicleDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedVehicle);
     }
     // Update Vehicle
     @PutMapping("/{vehicleId}")
     public ResponseEntity<VehicleDto> updateVehicle(@PathVariable String vehicleId, @RequestBody VehicleDto vehicleDto) {
+        log.info("Request received to update vehicle with ID: {}, Data: {}", vehicleId, vehicleDto);
         VehicleDto updatedVehicle = vehicleService.update(vehicleId, vehicleDto);
         return new ResponseEntity<>(updatedVehicle, HttpStatus.OK);
     }
     @DeleteMapping("/{vehicleId}")
     public ResponseEntity<String> deleteVehicle(@PathVariable("vehicleId") String vehicleId) {
         try{
+            log.info("Request received to delete vehicle with ID: {}", vehicleId);
             if (!RegexUtilForId.isValidVehicleId(vehicleId)){
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             } else {
@@ -46,7 +49,7 @@ public class VehicleController {
         }catch (VehicleNotFoundException e){
             return new ResponseEntity<>("vehicle not found.", HttpStatus.NOT_FOUND);
         }catch (Exception e){
-            e.printStackTrace();
+            log.error("Error deleting vehicle with ID: {}", vehicleId, e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
@@ -60,6 +63,7 @@ public class VehicleController {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<VehicleDto> getAllUsers(){
+        log.info("Request received to retrieve all vehicles.");
         return vehicleService.findAll();
     }
 
