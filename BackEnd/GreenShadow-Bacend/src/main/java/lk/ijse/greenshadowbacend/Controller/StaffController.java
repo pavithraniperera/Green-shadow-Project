422bb7da2444f8855bcc276af4d5de661848fe5c
pavithrani <pererapavithrani02@gmail.com>
2024-11-29 11:54:03 +0530

    update staff controllers with adding loggers

--- BackEnd/GreenShadow-Bacend/src/main/java/lk/ijse/greenshadowbacend/Controller/StaffController.java
+++ BackEnd/GreenShadow-Bacend/src/main/java/lk/ijse/greenshadowbacend/Controller/StaffController.java
@@ -6,6 +6,8 @@ import lk.ijse.greenshadowbacend.Exception.FieldNotFoundException;
 import lk.ijse.greenshadowbacend.Exception.StaffNotFoundException;
 import lk.ijse.greenshadowbacend.Service.StaffService;
 import lk.ijse.greenshadowbacend.Util.RegexUtilForId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,27 +22,34 @@ import java.util.Optional;
 @RequestMapping("api/v1/staffs")
 @CrossOrigin
 public class StaffController {
+    private static final Logger log = LoggerFactory.getLogger(StaffController.class);
     @Autowired
     private StaffService staffService;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATOR') or hasRole('SCIENTIST')")
     public ResponseEntity<StaffDto> saveStaff(@RequestBody StaffDto staffDto) {
+        log.info("Request received to save staff: {}", staffDto);
         StaffDto savedStaff = staffService.save(staffDto);
+        log.info("Staff saved successfully: {}", savedStaff);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedStaff);
     }
 
     @PutMapping(value = "/{staffId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATOR') or hasRole('SCIENTIST')")
     public ResponseEntity<StaffDto> updateStaff(@PathVariable("staffId") String staffId, @RequestBody StaffDto staffDto) {
+        log.info("Request received to update staff with ID: {}, Data: {}", staffId, staffDto);
         StaffDto updatedStaff = staffService.update(staffId, staffDto);
+        log.info("Staff updated successfully: {}", updatedStaff);
         return ResponseEntity.ok(updatedStaff);
     }
 
     @DeleteMapping("/{staffId}")
     @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATOR') or hasRole('SCIENTIST')")
     public ResponseEntity<String> deleteStaff(@PathVariable("staffId") String staffId) {
+
         try{
+            log.info("Request received to delete staff with ID: {}", staffId);
             if (!RegexUtilForId.isValidStaffId(staffId)){
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             } else {
@@ -50,7 +59,7 @@ public class StaffController {
         }catch (StaffNotFoundException e){
             return new ResponseEntity<>("Staff not found.", HttpStatus.NOT_FOUND);
         }catch (Exception e){
-            e.printStackTrace();
+            log.error("Error deleting staff with ID: {}", staffId, e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
@@ -59,13 +68,16 @@ public class StaffController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATOR') or hasRole('SCIENTIST')")
     public List<StaffDto> getAllUsers(){
+        log.info("Request received to retrieve all staff.");
         return staffService.findAll();
+
     }
 
     @GetMapping(value = "/{staffId}", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATOR') or hasRole('SCIENTIST')")
     public ResponseEntity<?> getFieldById(@PathVariable("staffId") String staffId) {
         // Validate field ID format using RegexUtilForId
+
         if (!RegexUtilForId.isValidStaffId(staffId)) {
             return new ResponseEntity<>( "Staff ID format is invalid", HttpStatus.BAD_REQUEST);
         }
@@ -82,14 +94,16 @@ public class StaffController {
     @GetMapping("/{staffId}/field")
     @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATOR') or hasRole('SCIENTIST')")
     public ResponseEntity<List<FieldDto>> getFieldsOfStaffId(@PathVariable("staffId") String staffId) {
+        log.info("Request received to retrieve fields for staff ID: {}", staffId);
         List<FieldDto> fieldDtos = staffService.getFieldsOfStaffId(staffId);
         return ResponseEntity.ok(fieldDtos);
     }
     @GetMapping("/email/{email}")
     @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATOR') or hasRole('SCIENTIST')")
     public ResponseEntity<StaffDto> getStaffByEmail(@PathVariable("email") String email) {
+        log.info("Staff retrieved successfully by email: {}", email);
         Optional<StaffDto> staffDto = staffService.findByEmail(email);
-
+        log.warn("Staff not found with email: {}", email);
         return ResponseEntity.ok(staffDto.get());
     }
 
